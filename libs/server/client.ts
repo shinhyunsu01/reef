6af7bcaee0106cfcefa8b926c80abccf6bf37df6
shine9687 <sinhyeonsu@MacBook-Pro.local>
2022-07-14 23:49:28 +0900

    test2

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -5,6 +5,7 @@ declare global {
 }
 const client = global.client || new PrismaClient();
 
-if (process.env.NODE_ENV === "development") global.client = client;
+//if (process.env.NODE_ENV === "development")
+global.client = client;
 
 export default client;
