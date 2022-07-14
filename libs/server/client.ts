7c8842d2cec99c252c16b1045264235bfd2d214c
shine9687 <sinhyeonsu@MacBook-Pro.local>
2022-07-14 23:58:39 +0900

    test60

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -7,5 +7,5 @@ const client = new PrismaClient();
 
 //if (process.env.NODE_ENV === "development")
 //global.client = client;
-
+console.log("process.env.NODE_ENV", process.env.NODE_ENV);
 export default client;
