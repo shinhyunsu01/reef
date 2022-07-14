8096f9ba82a4b9ad6641abbadc8ca8e4dcf36d3c
shine9687 <sinhyeonsu@MacBook-Pro.local>
2022-07-15 08:56:47 +0900

    t21

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -104,7 +104,7 @@ export default function Navbar() {
 		const login = () => {
 			naverLogin = new naver.LoginWithNaverId({
 				clientId: process.env.NEXT_PUBLIC_NAVER_LOGIN_ID, // ClientID
-				callbackUrl: "https://reefcrew.vercel.app/", // Callback URL
+				callbackUrl: "https://reef-nine.vercel.app/#", // Callback URL
 				isPopup: false, // 팝업 형태로 인증 여부
 				loginButton: { color: "green", type: 1, height: 10 },
 			});
