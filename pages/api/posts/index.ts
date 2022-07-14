ed9244936f696ddced7bbc09e3e1eed714d4bc8b
shine9687 <sinhyeonsu@MacBook-Pro.local>
2022-07-15 01:12:55 +0900

    t12

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -10,28 +10,28 @@ async function handler(
 	let manyUser;
 	if (req.method === "GET") {
 		console.log("aa");
-		//client.$queryRaw`SET SESSION sql_mode = 'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';`.then(
-		//async () => {
-		manyUser = await client.user.findMany({
-			select: {
-				id: true,
-				nickname: true,
-				avatar: true,
-				backavatar: true,
-			},
-		});
-		if (manyUser) {
-			res.json({
-				ok: true,
-				users: manyUser,
-			});
-		} else {
-			res.json({
-				ok: false,
-			});
-		}
-		//}
-		//);
+		client.$queryRaw`SET SESSION sql_mode = 'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';`.then(
+			async () => {
+				manyUser = await client.user.findMany({
+					select: {
+						id: true,
+						nickname: true,
+						avatar: true,
+						backavatar: true,
+					},
+				});
+				if (manyUser) {
+					res.json({
+						ok: true,
+						users: manyUser,
+					});
+				} else {
+					res.json({
+						ok: false,
+					});
+				}
+			}
+		);
 	}
 }
 
