33d0b0239e92285adca380f08fe26a0bc9263f2f
shine9687 <sinhyeonsu@MacBook-Pro.local>
2022-07-15 02:18:06 +0900

    t19

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,7 +23,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 					<Head>
 						<script
 							src="https://static.nid.naver.com/js/naveridlogin_js_sdk_2.0.2.js"
-							async
+							defer
 						/>
 					</Head>
 					<Component {...pageProps} />
