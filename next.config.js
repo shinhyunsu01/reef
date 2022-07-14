02237b9e902874bef4797762ccbc3fb2fb2f53f0
shine9687 <sinhyeonsu@MacBook-Pro.local>
2022-07-14 23:32:51 +0900

    test39

--- next.config.js
+++ next.config.js
@@ -12,13 +12,9 @@ const nextConfig = {
 	images: {
 		domains: ["imagedelivery.net", "upload.imagedelivery.net"],
 	},
-	env: {
-		DATABASE_URL: process.env.NEXT_PUBLIC_DATABASE_URL,
-		COOKIE_PASSWORD: process.env.NEXT_PUBLIC_COOKIE_PASSWORD,
-		NAVER_LOGIN_ID: process.env.NEXT_PUBLIC_NAVER_LOGIN_ID,
-		NAVER_LOGIN_PASSWORD: process.env.NEXT_PUBLIC_NAVER_LOGIN_PASSWORD,
-		CF_TOKEN: process.env.NEXT_PUBLIC_CF_TOKEN,
-		CF_ID: process.env.NEXT_PUBLIC_CF_ID,
+
+	experimental: {
+		reactRoot: true,
 	},
 };
 
