564c52bd379436c108964a61f85dab42c489b838
shine9687 <sinhyeonsu@MacBook-Pro.local>
2022-07-15 10:33:13 +0900

    t25

--- pages/index.tsx
+++ pages/index.tsx
@@ -63,10 +63,10 @@ interface ManyUser {
 }
 
 const Index = () => {
-	/*const { data, error } = useSWR<ManyUser>(
+	const { data, error } = useSWR<ManyUser>(
 		typeof window === "undefined" ? null : "/api/posts"
 	);
-	console.log(process.env.NODE_ENV, data, error);*/
+	console.log(process.env.NODE_ENV, data, error);
 
 	return (
 		<Main>
