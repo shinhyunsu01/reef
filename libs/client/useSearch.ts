ee9d1ba577f45fced6178ac53b0ecbbee645710f
shine9687 <sinhyeonsu@MacBook-Pro.local>
2022-07-20 00:02:53 +0900

    deploy error check 24

--- libs/client/useSearch.ts
+++ libs/client/useSearch.ts
@@ -2,6 +2,6 @@ import useSWR from "swr";
 
 export default function useSearch() {
 	const { data, error } = useSWR("/api/search");
-
+	console.log(data);
 	return { data, isLoading: !data && !error };
 }
