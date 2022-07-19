66b9b3de78314724f85dd38849839fe56fc4b17f
shine9687 <sinhyeonsu@MacBook-Pro.local>
2022-07-19 23:28:14 +0900

    deploy error check 20

--- pages/api/search/index.ts
+++ pages/api/search/index.ts
@@ -1,16 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "../../../libs/server/withHandler";
-import { withIronSessionApiRoute } from "iron-session/next";
 
 import client from "../../../libs/server/client";
-
-declare module "iron-session" {
-	interface IronSessionData {
-		user?: {
-			id: number;
-		};
-	}
-}
 
 async function handler(
 	req: NextApiRequest,
@@ -41,10 +32,6 @@ async function handler(
 	}
 }
 
-export default withIronSessionApiRoute(
-	withHandler({ methods: ["GET", "POST"], handler }),
-	{
-		cookieName: "reef",
-		password: process.env.NEXT_PUBLIC_API_COOKIE!,
-	}
+export default 
+	withHandler({ methods: ["GET", "POST"], handler })
 );
