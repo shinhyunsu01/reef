220ac1754ebe853f8eb81ec1eea0486cc9c76601
shine9687 <sinhyeonsu@MacBook-Pro.local>
2022-07-19 22:45:35 +0900

    deploy error check 08

--- pages/index.tsx
+++ pages/index.tsx
@@ -95,9 +95,44 @@ const Page: NextPage<ManyUser> = ({ users }) => {
 	};
 	return (
 		<Main>
-			<Navbar />
 			<PicBody>
-				<div>asf</div>
+				{users
+					? users.map((data, i) => (
+							<Pic
+								key={i}
+								onClick={() => {
+									onClick(data.id);
+								}}
+							>
+								<PicTitle>
+									<ShowAvatar
+										data={data?.avatar}
+										layout="responsive"
+										width={100}
+										height={100}
+									/>
+									{data.nickname}
+								</PicTitle>
+
+								{data.backavatar ? (
+									<Image
+										layout="responsive"
+										width={100}
+										height={100}
+										src={`https://imagedelivery.net/fhkogDoSTeLvyDALpsIbnw/${data?.backavatar}/public`}
+									/>
+								) : (
+									<Image
+										layout="responsive"
+										width={100}
+										height={100}
+										src={backInitImg}
+										placeholder="blur"
+									/>
+								)}
+							</Pic>
+					  ))
+					: ""}
 			</PicBody>
 		</Main>
 	);
