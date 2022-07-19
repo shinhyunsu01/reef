0d647f8ca1ef172190d560deff34151702bc6ee9
shine9687 <sinhyeonsu@MacBook-Pro.local>
2022-07-19 21:16:29 +0900

    bug fix

--- components/User/avatar.tsx
+++ components/User/avatar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-import Image from "Next/image";
+import Image from "next/image";
 
 const AvatarImage = styled.div`
 	height: 4rem;
